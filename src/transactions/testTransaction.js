4a147621fc9d6e1c4e37c49224b4d418c878e0a0
thepablosantos <iphonepablosantos@gmail.com>
2025-03-20 12:04:39 +0000

    feat: fetch block and transaction details

--- src/transactions/testTransaction.js
+++ src/transactions/testTransaction.js
@@ -1,6 +1,6 @@
 const { getTransaction, getTransactionReceipt } = require("./getTransactionDetails");
 
-// Use um hash de transação válido da sua testnet!
+// Put a valid transaction hash from your testnet!
 const txHash = "0x26183ee23194ca07e316daab81b117a740c35b8edfab21349c142a3eb1f68a95";
 
 getTransaction(txHash);
